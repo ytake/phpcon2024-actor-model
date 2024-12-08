dde2c256631d50643562f9d2c302601528cf5ca3
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2024-12-09 00:53:45 +0900

    added example

--- calculator/src/Calculator/PersistenceActor.php
+++ calculator/src/Calculator/PersistenceActor.php
@@ -4,7 +4,25 @@ declare(strict_types=1);
 
 namespace Calculator;
 
-class PersistenceActor
-{
+use Google\Protobuf\Internal\Message;
+use Phluxor\ActorSystem\Context\ContextInterface;
+use Phluxor\ActorSystem\Message\ActorInterface;
+use Phluxor\Persistence\Mixin;
+use Phluxor\Persistence\PersistentInterface;
 
+class PersistenceActor implements ActorInterface, PersistentInterface
+{
+    use Mixin;
+
+    public function receive(ContextInterface $context): void
+    {
+        // TODO: Implement receive() method.
+    }
+
+    private function updateState(Message $message): void
+    {
+        if (!$this->recovering()) {
+            $this->persistenceReceive($message);
+        }
+    }
 }
